212be030fa5820973813ce84a75c0e35a82cc844
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-01-26 22:21:30 +0100

    feat (html) /thumbnails

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,31 +6,59 @@
     // 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 //
 
-// Creazione Array Oggetti 
-let songArray = [
-    {
-        img: "./assets/img/bring-me-the-horizon.jpg",
-        title: "Can You Feel My Heart?", 
-        author: "By: Bring Me The Horizon"
+
+const { createApp } = Vue  
+
+createApp({
+    data(){
+        return{
+            currentSong: 0,
+            autoscroll: null, 
+            songArray: [
+                {
+                    img: './assets/img/bring-me-the-horizon.jpg',
+                    title: "Can You Feel My Heart?", 
+                    author: "By: Bring Me The Horizon"
+                },
+                {
+                    img: "./assets/img/the-dresden-dolls.jpg",
+                    title: "My Alcoholic Friends", 
+                    author: "By: The Dresden Dolls"
+                },
+                {
+                    img: "./assets/img/the-neighbourhood.jpeg",
+                    title: "Softcore", 
+                    author: "By: The Neighbourhood"
+                },
+                {
+                    img: "./assets/img/mother-mother.jpg",
+                    title: "Hayloft II", 
+                    author: "By: Mother Mother"
+                },
+                {
+                    img: "./assets/img/my-chemical-romance.jpg",
+                    title: "Teenagers", 
+                    author: "By: My Chemical Romance"
+                },
+            ]
+        }
     },
-    {
-        img: "./assets/img/the-dresden-dolls.jpg",
-        title: "My Alcoholic Friends", 
-        author: "By: The Dresden Dolls"
+    methods:{
+        previousSong(){
+            if (this.currentSong === 0){
+                this.currentSong = this.songArray.length
+            }
+
+            this.currentSong--
+
+        },
+        nextSong(){
+            this.currentSong++
+
+            if (this.currentSong > this.songArray.length - 1){
+                this.currentSong = 0
+            }
+        },
     },
-    {
-        img: "./assets/img/the-neighbourhood.jpeg",
-        title: "Softcore", 
-        author: "By: The Neighbourhood"
-    },
-    {
-        img: "./assets/img/mother-mother.jpg",
-        title: "Hayloft II", 
-        author: "By: Mother Mother"
-    },
-    {
-        img: "./assets/img/my-chemical-romance.jpg",
-        title: "Teenagers", 
-        author: "By: My Chemical Romance"
-    },
-]
+    
+}).mount("#main-container") 
